0af4c3bdc72f8794fc67cb7a846e3935e3a72f5e
nuttaponkhamkul123 <aladen0009@gmail.com>
2025-06-04 11:41:29 +0700

    add basic structure to project

---
+++ frontend/src/components/header/index.jsx
@@ -0,0 +1,15 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import './App.css'
+
+function Header() {
+
+  return (
+    <>
+      hello header
+    </>
+  )
+}
+
+export default Header
